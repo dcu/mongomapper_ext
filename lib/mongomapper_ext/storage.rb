b6b7962985276b20c269b57d8e187f3fd3887f9f
David A. Cuadrado <krawek@gmail.com>
2010-04-10 03:36:11 -0500

    add some validations before storing the file
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/mongomapper_ext/storage.rb
+++ lib/mongomapper_ext/storage.rb
@@ -4,6 +4,7 @@ module MongoMapperExt
       model.class_eval do
         extend ClassMethods
 
+        validate :add_mm_storage_errors
         file_list :file_list
       end
     end
@@ -21,6 +22,18 @@ module MongoMapperExt
     def files(options = {})
       file_list = send(options.delete(:in) || :file_list)
       file_list.files
+    end
+
+    def mm_storage_errors
+      @mm_storage_errors ||= {}
+    end
+
+    def add_mm_storage_errors
+      mm_storage_errors.each do |k, msgs|
+        msgs.each do |msg|
+          self.errors.add(k, msg)
+        end
+      end
     end
 
     module ClassMethods
@@ -50,7 +63,24 @@ module MongoMapperExt
         opts[:in] ||= :file_list
 
         define_method("#{name}=") do |file|
-          send(opts[:in]).get(name.to_s).put(name.to_s, file)
+          if opts[:max_length] && file.respond_to?(:size) && file.size > opts[:max_length]
+            errors.add(name, I18n.t("mongomapper_ext.storage.errors.max_length", :default => "file is too long. max length is #{opts[:max_length]} bytes"))
+          end
+
+          if cb = opts[:validate]
+            if cb.kind_of?(Symbol)
+              send(opts[:validate], file)
+            elsif cb.kind_of?(Proc)
+              cb.call(file)
+            end
+          end
+
+          if self.errors.on(name).blank?
+            send(opts[:in]).get(name.to_s).put(name.to_s, file)
+          else
+            # we store the errors here because we want to validate before storing the file
+            mm_storage_errors.merge!(errors.errors)
+          end
         end
 
         define_method(name) do
